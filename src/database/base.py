c3e67fb2304a184ec29f8b6dedda239f9db90a37
KsQRooW <dimankochetov@yandex.ru>
2023-10-14 00:13:33 +0300

    feat(database): добавлено поле id как базовое поле для всех ORM моделей

--- src/database/base.py
+++ src/database/base.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 engine = create_engine("sqlite://", echo=True)
 
 
 class Base(DeclarativeBase):
-    """Базовый класс ORM моделей"""
+    """Базовый класс ORM моделей
+
+    :param id: primary key
+    """
+    id: Mapped[int] = mapped_column(primary_key=True)
